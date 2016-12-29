21cd1ca6ab33b7b61da452072759842656bcb143
Adrian Sroka <adrian.sroka@britishcouncil.pl>
2016-12-29 15:03:14 +0100

    Deployment

--- src/gulp/test.js
+++ src/gulp/test.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var path = require('path');
 var tslint = require("gulp-tslint");
+var gulpIgnore = require('gulp-ignore');
 
 var conf = require('../../base/gulp/conf');
 
@@ -15,6 +16,7 @@ gulp.task('tslint', () => {
   console.log('App-specific version of tslint');
 
   return gulp.src(tsLinterSrc)
+    .pipe(gulpIgnore.exclude('./app/services/serviceClient.ts'))
     .pipe(tslint({ formatter: "verbose" }))
     .pipe(tslint.report())
 });
